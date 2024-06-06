cc4ffe5db9ad2ea3b7c7500c54ed58a33485afd4
Jonathan <byersj920@gmail.com>
2024-06-06 18:05:36 -0500

    Made the fetch async

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,9 +1,33 @@
-fetch('http://localhost:8080/https://api2.moxfield.com/v3/decks/all/lUbVQLCaMk2I8lsCi9pzLw')
-  .then(response => {
+let deckData = {};
+
+async function fetchDeckData() {
+  try {
+    const response = await fetch('http://localhost:8080/https://api2.moxfield.com/v3/decks/all/lUbVQLCaMk2I8lsCi9pzLw');
     if (!response.ok) {
       throw new Error('Network response was not ok ' + response.statusText);
     }
-    return response.json();
-  })
-  .then(data => console.log(data))
-  .catch(error => console.error('There has been a problem with your fetch operation:', error));
+    const data = await response.json();
+    deckData = data;
+    console.log(deckData);
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+async function initialize() {
+  await fetchDeckData();
+  console.log(deckData);
+  console.log(deckData.name);
+
+
+
+
+
+
+  
+
+
+
+}
+
+initialize();
