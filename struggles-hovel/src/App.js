53c1da647fae615c08be1ff6e477f4d6ac323c64
Jonathan <byersj920@gmail.com>
2024-06-30 13:08:59 -0500

    Attempted to fix vite errors

---
+++ struggles-hovel/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TestCode from './Components/TestCode';
+
+function App() {
+  return (
+    <div className="App">
+      <TestCode />
+    </div>
+  );
+}
+
+export default App;
