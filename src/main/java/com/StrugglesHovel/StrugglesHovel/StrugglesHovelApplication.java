22810f81414bdb6a525987565f7a4c7f4e3437c9
Jonathan <byersj920@gmail.com>
2024-09-03 10:39:32 -0500

    Cleaned up backend organization

---
+++ src/main/java/com/StrugglesHovel/StrugglesHovel/StrugglesHovelApplication.java
@@ -0,0 +1,12 @@
+package com.StrugglesHovel.StrugglesHovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrugglesHovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrugglesHovelApplication.class, args);
+	}
+}
