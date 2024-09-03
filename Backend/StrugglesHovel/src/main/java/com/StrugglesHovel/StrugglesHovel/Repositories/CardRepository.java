25657ef54198ec8267dd501c02b3fa50e9a98d6d
Jonathan <byersj920@gmail.com>
2024-09-03 10:39:13 -0500

    Cleaned up backend organization

--- Backend/StrugglesHovel/src/main/java/com/StrugglesHovel/StrugglesHovel/Repositories/CardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.StrugglesHovel.StrugglesHovel.Repositories;
-
-import com.StrugglesHovel.StrugglesHovel.Models.Card;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardRepository extends JpaRepository<Card, Long> {
-}
--- Backend/StrugglesHovel/src/main/java/com/StrugglesHovel/StrugglesHovel/Repositories/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.StrugglesHovel.StrugglesHovel.Repositories;
-import com.StrugglesHovel.StrugglesHovel.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
